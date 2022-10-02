c3ea89fec38eb90f475aaa4331da2cbd70c7ec37
Adam Giacomelli <adam.giacom@gmail.com>
2022-10-02 14:58:11 +0200

    Added random problems generator

--- src/utils/gameState.js
+++ src/utils/gameState.js
@@ -1,7 +1,92 @@
+/* eslint-disable max-classes-per-file */
+import { Input } from 'phaser';
 import { BROKEN_OBJECT_SPRITE_NAME, TILE_HEIGHT, TILE_WIDTH } from '../constants';
+import { createInteractiveGameObject } from './utils';
 
 const START_TIME = 32527384553000;
 const DECIMALS = 2;
+
+const locations = [
+    { x: 12, y: 12 },
+    { x: 11, y: 12 },
+    { x: 10, y: 12 },
+    { x: 9, y: 12 },
+    { x: 8, y: 12 },
+    { x: 7, y: 12 },
+    { x: 12, y: 10 },
+    { x: 12, y: 11 },
+];
+
+class Modifier {
+    repairNeeded = 1
+    constructor(scene, position, repairNeeded) {
+        this.repairNeeded = repairNeeded ?? 1;
+        this.createBrokenObject(scene, position);
+    }
+
+    repair() {
+        this.repairNeeded -= 1;
+    }
+
+    isFixed() {
+        return this.repairNeeded <= 0;
+    }
+
+    createBrokenObject(scene, position) {
+        const x = position.x * TILE_WIDTH;
+        const y = position.y * TILE_HEIGHT;
+        const objectIndex = 1;
+        const name = `${BROKEN_OBJECT_SPRITE_NAME}_${objectIndex}`;
+        const brokenObject = scene.physics.add
+            .sprite(x, y, BROKEN_OBJECT_SPRITE_NAME)
+            .setOrigin(0, 0)
+            .setName(name)
+            .setDepth(10);
+
+        const animationKey = `${BROKEN_OBJECT_SPRITE_NAME}_idle`;
+        if (!scene.anims.exists(animationKey)) {
+            scene.anims.create({
+                key: animationKey,
+                frames: Array.from({ length: 2 }).map((n, index) => ({
+                    key: BROKEN_OBJECT_SPRITE_NAME,
+                    frame: `${BROKEN_OBJECT_SPRITE_NAME}_idle_${(index + 1).toString().padStart(2, '0')}`,
+                })),
+                frameRate: 3,
+                repeat: -1,
+                yoyo: false,
+            });
+        }
+
+        brokenObject.anims.play(animationKey);
+
+        scene.items.add(brokenObject);
+
+        const customCollider = createInteractiveGameObject(
+            scene,
+            x,
+            y,
+            TILE_WIDTH,
+            TILE_HEIGHT
+        );
+
+        const overlapCollider = scene.physics.add.overlap(
+            scene.heroSprite.actionCollider,
+            customCollider,
+            () => {
+                if (Input.Keyboard.JustDown(scene.actionKey)) {
+                    console.log('Repairing');
+                    this.repair();
+                    if (this.isFixed()) {
+                        scene.physics.world.removeCollider(overlapCollider);
+                        customCollider.destroy();
+                        brokenObject.setVisible(false);
+                        brokenObject.destroy();
+                    }
+                }
+            }
+        );
+    }
+}
 
 export default class GameState {
   hud = {};
@@ -15,12 +100,19 @@ export default class GameState {
 
   modifiers = [];
 
+  timeleft = 10;
+
   constructor(scene) {
       this.hud.timedate = scene.add.text(0, 0, this.getTimeText(), {
           font: '"Press Start 2P"',
       });
       this.hud.resources = scene.add.text(0, 30, this.getHudText(), {
           font: '"Press Start 2P"',
+      });
+
+      this.hud.timerText = scene.add.text(TILE_WIDTH * 15, 0, `Next problem in ${10}`, {
+          font: '"Press Start 2P"',
+          color: '#FF0000',
       });
 
       this.resourceState = {
@@ -30,7 +122,17 @@ export default class GameState {
           fuel: 100,
       };
 
-      this.createBrokenObject(scene, { x: 10, y: 10 });
+      setInterval(() => {
+          if (this.timeleft === 0) {
+              this.timeleft = 11;
+
+              const random = Math.floor(Math.random() * locations.length);
+              this.modifiers.push(new Modifier(scene, locations[random], 3));
+              const random2 = Math.floor(Math.random() * locations.length);
+              this.modifiers.push(new Modifier(scene, locations[random2], 3));
+          }
+          this.timeleft -= 1;
+      }, 1000);
   }
 
   getTimeText = (time) => {
@@ -51,52 +153,7 @@ export default class GameState {
       this.resourceState.fuel -= 0.00001 * delta;
       this.hud.resources.setText(this.getHudText());
       this.hud.timedate.setText(this.getTimeText(time));
-  }
 
-  createBrokenObject = (scene, position) => {
-      const { x, y } = position;
-      const objectIndex = 1;
-      const name = `${BROKEN_OBJECT_SPRITE_NAME}_${objectIndex}`;
-      const brokenObject = scene.physics.add
-          .sprite(x * TILE_WIDTH, y * TILE_HEIGHT, BROKEN_OBJECT_SPRITE_NAME)
-          .setOrigin(0, 0)
-          .setName(name)
-          .setDepth(10);
-
-      const animationKey = `${BROKEN_OBJECT_SPRITE_NAME}_idle`;
-      if (!scene.anims.exists(animationKey)) {
-          scene.anims.create({
-              key: animationKey,
-              frames: Array.from({ length: 2 }).map((n, index) => ({
-                  key: BROKEN_OBJECT_SPRITE_NAME,
-                  frame: `${BROKEN_OBJECT_SPRITE_NAME}_idle_${(index + 1).toString().padStart(2, '0')}`,
-              })),
-              frameRate: 3,
-              repeat: -1,
-              yoyo: false,
-          });
-      }
-
-      brokenObject.anims.play(animationKey);
-
-      scene.items.add(brokenObject);
-
-      //   const customCollider = createInteractiveGameObject(
-      //       scene,
-      //       x,
-      //       y,
-      //       TILE_WIDTH,
-      //       TILE_HEIGHT,
-      //       { x, y }
-      //   );
-
-      //   const overlapCollider = scene.physics.add.overlap(
-      //       scene.heroSprite,
-      //       customCollider,
-      //       () => {
-      //           scene.physics.world.removeCollider(overlapCollider);
-      //           console.log('gotit');
-      //       }
-      //   );
+      this.hud.timerText.setText(`Next problem in ${this.timeleft}`);
   }
 }
